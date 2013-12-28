ec8e4809136c9b9cbe37af4752e014cedeaf4b6b
Christiaan <christiaan@k75.(none)>
2013-12-28 22:31:49 +0200

    convert, shield, deflecct working

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 //Global variables
 var players = 0;
+var player_names = [];
+var grid = [];
+var queue = [];
+var nextQueue = [];
+var weaponsBank = [];
+
+
+var weapons1 = ["shield", "convert"];
+var weapons2 = ["deflect", "steal", "rotten"];
+var weapons3 = ["nuke", "emp", "kamikaze"];
 
 //setup Dependencies
 var connect = require('connect')
@@ -29,6 +39,24 @@ stdin.addListener("data", function(d)
     //Starting game
     console.log("Starting server.");
     StartServer();
+  }
+  if(data == "grid") {
+    //Show grid
+    for(var i = 0; i < grid.length; i++)
+    {
+      console.log((i+1) + ": " + grid[i].owner + ", " + grid[i].remark);
+    }
+  }
+  if(data == "weapons") {
+    for(var i = 0; i < weaponsBank.length; i++)
+    {
+      var w = weaponsBank[i];
+      console.log("Player: " + w.owner);
+      console.log("Weapons: " + w.weapons);
+    }
+  }
+  if(data == "players") {
+    console.log(players);
   }
 });
 
@@ -64,10 +92,35 @@ io.sockets.on('connection', function(socket){
   });
   socket.on('login', function(data) {
     console.log(data + " has connected.");
+    if(player_names.lastIndexOf(data) >= 0) {
+      socket.emit('invalid-name');
+      return;
+    }
     socket.emit('login-success');
     socket.set('name', data);
+    player_names.push(data);
+    console.log("player names now: " + player_names);
     players++;
   });
+
+  socket.on('shield-me', function(name)
+  {
+      shieldPlayer(name);
+      removeWeapon("shield", name);
+  });
+
+  socket.on('deflect-me', function(name)
+  {
+      deflectPlayer(name);
+      removeWeapon("deflect", name);
+  });
+
+  socket.on('convert', function(name)
+  {
+      convertRandomBlock(name);
+      removeWeapon("convert", name);
+  });
+
   socket.on('disconnect', function(){
     console.log('Client Disconnected.');
   });
@@ -91,6 +144,32 @@ server.get('/', function(req,res){
   });
 });
 
+server.get('/status/:number', function(req, res)
+{
+    var num = req.params.number;
+    if (num < 1 || num > 36) {
+      res.write("invalid");
+    } else {
+      var owner = grid[num - 1].owner;
+      res.write(owner);
+    }
+    res.end();
+});
+
+server.get('/weapons/:name', function(req, res)
+{
+    console.log("Received weapons request");
+    var name = req.params.name;
+    for(var i = 0; i < weaponsBank.length; i++)
+    {
+        var w = weaponsBank[i];
+        if(w.owner == name) {
+          res.write(JSON.stringify(w.weapons));
+        }
+    }
+    res.end();
+});
+
 //A Route for Creating a 500 Error (Useful to keep around)
 server.get('/500', function(req, res){
     throw new Error('This is a 500 Error');
@@ -111,14 +190,206 @@ function NotFound(msg){
 console.log('Listening on http://0.0.0.0:' + port );
 
 ///////////////////////////////////////////
+//              Helpers                  //
+///////////////////////////////////////////
+
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+///////////////////////////////////////////
 //              Whacker                  //
 ///////////////////////////////////////////
 
 function StartServer()
 {
-   io.sockets.clients().forEach(function (socket) {
 
-    
-      socket.emit('starting', players);  
+   assignWeapons();
+   populateGrid();
+   
+   io.sockets.clients().forEach(function (socket) {
+      socket.emit('starting', player_names.join(", "));  
     });
+}
+
+
+function shieldPlayer(name)
+{
+  var blocks = [];
+  for(var i = 0; i < grid.length; i++)
+  {
+      if(grid[i].owner == name && grid[i].remark != "dead") {
+        blocks.push(i);
+      }
+  }
+
+  var index = blocks[Math.floor(Math.random() * blocks.length)];
+  grid[index].remark = "shield";
+  console.log("Shielded " + name + " at block " + (index + 1));
+}
+
+function deflectPlayer(name)
+{
+  var blocks = [];
+  for(var i = 0; i < grid.length; i++)
+  {
+      if(grid[i].owner == name && grid[i].remark == "none") {
+        blocks.push(i);
+      }
+  }
+
+  var index = blocks[Math.floor(Math.random() * blocks.length)];
+  grid[index].remark = "deflect";
+  console.log("Deflectorized " + name + " at block " + (index + 1));
+}
+
+function convertRandomBlock(name)
+{
+    var blocks = [];
+    var originalOwner = "";
+    for(var i = 0; i < grid.length; i++)
+    {
+        if(grid[i].remark == "none" && grid[i].owner != name) {
+          blocks.push(i);
+        }
+    }
+
+    console.log("Converting! Blocks = ");
+    for(var i = 0; i < blocks.length; i++)
+    {
+      console.log((i+1) + ": Curr owner = " + grid[i].owner);
+    }
+
+
+    var index = blocks[Math.floor(Math.random() * blocks.length)];
+    originalOwner = grid[index].owner;
+    grid[index].owner = name;
+    console.log("Block " + (index + 1) + " now belongs to " + name);
+    //Notify player of his new block!
+    io.sockets.clients().forEach(function (socket) {
+      socket.get('name', function(err, pname) {
+        console.log("pname = " + pname);
+        if(pname == name) {
+          socket.emit('converted-gain', (index+1));
+        } else if (pname == originalOwner) {
+          socket.emit('converted-loss', (index+1));
+        }
+      });
+      
+    });
+    var msg = name + ' stole a block from ' + originalOwner;
+    messageAll(msg);
+    
+}
+
+function messageAll(message) {
+  io.sockets.clients().forEach(function (socket) {
+      socket.emit('message', message);  
+    });
+}
+
+function populateGrid()
+{
+    var perPlayer = Math.round(36/players);
+    var index = 0;
+    for(var i = 0; i < players; i++) {
+      for(var j = 0; j < perPlayer; j++) {
+
+        grid.push({owner: player_names[i], remark: "none"});
+      }
+    }
+
+    var blanks = 36 - grid.length;
+    for(var i = 0; i < blanks; i++)
+    {
+      grid.push({owner: "Blank", remark: "None"});
+    }
+
+    shuffle(grid);
+
+}
+
+function assignWeapons()
+{
+
+  console.log("Assigning weapons");
+  player_names.forEach(function(player)
+  {
+      console.log("Player = " + player);
+      var weapons = [];
+      // Level 1
+      weapons.push(weapons1[0]);
+      weapons.push(weapons1[1]);
+
+      //Level 2
+      for(var i = 0; i < 2; i++)
+      {
+        weapons.push(weapons2[Math.floor(Math.random() * weapons2.length)]);
+      }
+
+      //Level 3
+      for(var i = 0; i < 2; i++)
+      {
+        weapons.push(weapons3[Math.floor(Math.random() * weapons3.length)]);
+      }
+
+      console.log("Weapons = " + weapons);
+
+      weaponsBank.push({
+        owner: player,
+        weapons: weapons
+      });
+  });
+}
+
+function removeWeapon(weapon, name)
+{
+    for(var i = 0; i < weaponsBank.length; i++)
+    {
+        var w = weaponsBank[i];
+        if(w.owner == name) {
+           var weapons = w.weapons;
+           var index = weapons.lastIndexOf(weapon);
+           weapons.splice(index, 1);
+           w.weapons = weapons;
+        }
+
+    }
+    
+    console.log("Removed " + weapon + " from " + name);
+}
+
+///////////////////////////////////////////
+//       queue system                    //
+///////////////////////////////////////////
+
+/*
+    List of instructions:
+
+    kill [x] [killer] [msg] -> kill block x
+    killrandom [killer] [msg] -> kill random alive block
+    killadj [x] [killer] [msg] -> kills blocks adjacent to x (not x)
+    defer [x] [command] -> defer command for x turns
+    
+    note: killer is the name of the killer
+    note: killadj gets converted to 8 kills
+    note: killrandom gets converted to kill
+    note: defer [x] [cmd] gets converted to defer [x-1] [cmd] 
+    note: msg will be broadcasted to all players
+*/
+
+/* Queue system ;
+   1. originates at processQueue
+   2. calls execute[command]
+   3. execute[command] calls processQueue
+   4. once queue is empty, next player's turn
+*/
+
+
+function executeKill (x, killer, msg)
+{
+    //kill [x] [msg]
+    //ex 'kill 4 "A killed B"'
+
 }
--- static/js/script.js
+++ static/js/script.js
@@ -5,10 +5,21 @@
 
 var name = "";
 var controller;
+var queries = 3;
+
+var myTurn = true;
+
+
+var inQuery = false;
+
+var socket;
+
+var msg_query = "Select a block to query.";
+var msg_end = "Type 'cancel' to cancel action.";
 
 $(document).ready(function() {   
 
-  var socket = io.connect();
+  socket = io.connect();
 
   $('#submit-btn').bind('click', function() {
   	name = $("#player-name").val();
@@ -23,6 +34,11 @@ $(document).ready(function() {
    $('#receiver').append('<li>' + data + '</li>');  
   });
 
+  socket.on('invalid-name', function() {
+    alert("Name already in use, please pick another one");
+    $("#player-name").val("");
+  });
+
   socket.on('login-success', function()
   {
   	console.log("received handshake");
@@ -30,20 +46,41 @@ $(document).ready(function() {
   	$('#player-name').prop("disabled", true);
   });
 
+  socket.on('converted-gain', function(index)
+  {
+    console.log("Got converted signal.");
+    $("#button" + index).removeClass("btn-success");
+    $("#button" + index).addClass("btn-primary");
+    setButtonText(index, name);
+  });
+
+  socket.on('converted-loss', function(index)
+  {
+    $("#button" + index).removeClass("btn-success");
+    $("#button" + index).removeClass("btn-primary");
+    $("#button" + index).addClass("btn-success");
+    setButtonText(index, index);
+    showMessage("You lost a block, too bad.");
+  });
+
+  socket.on('message', function(msg)
+  {
+    showMessage(msg);
+  });
+
   socket.on('starting', function(players)
   {
-    setupGrid();
+    setupGrid(players);
 
   });
 });
 
 
-function setupGrid()
+function setupGrid(players)
 {
     var html = new EJS({url: '/templates/grid.ejs'}).render();
     $('.main-container').html(html);  
-    fillBlocks();
-    buildConsole();
+    buildConsole(players);
     $("#message-box").attr("disabled", "disabled");
     fillBlocks(36);
 }
@@ -56,29 +93,59 @@ function fillBlocks(n)
    {
      var label = "";
      if(i == k) label = "Mine";
-     $('#battlezone').append('<div class="btn btn-success grid odd"><div class="coord">' + label + ' </div></div>');
+     var buttonID = "button" + (i + 1);
+     var buttonHTML = new EJS({url: '/templates/button.ejs'}).render({label: label, num: (i+1)});
+     $('#battlezone').append(buttonHTML);
+     
    }
   
 }
 
-function buildConsole()
+function buildConsole(players)
 {
+  
+  var welcome = "Welcome to Whacker, " + name + ".\n Your opponents are: " + players + ".";
+
    var console = $('<div class="console1">');
    $('#console-container').append(console);
    controller = console.console({
       promptLabel: 'Whacker > ',
-      welcomeMessage: 'Welcome to Whacker, ' + name + '.',
+      welcomeMessage: welcome,
       commandHandle: function(line) {
          if (line) {
               //return [{msg:"you typed " + line,className:"jquery-console-message-value"}];
+              var msg = "Affirmative.\n Second line?";
+              var className = "jquery-console-message-value";
+              var parts = line.split(" ");
               if(line == "clear") {
                 controller.reset();
                 return;
+              } else if(line == "queries") {
+                msg = "You have " + queries + " queries left.";
+              } 
+
+              else if(line == "cancel")
+              {
+                //set all in variables to false
+                inQuery = false;
+                msg = "Action cancelled.";
+              }
+
+              else if(parts[0] == "use") 
+              { 
+                var result = processCommand(parts);
+                msg = result;
+              }
+              else if(line == "weapons") {
+                var weapons = showWeapons();
+                msg = weapons;
+              }
+              else {
+                msg = "Invalid command.";
               }
 
 
-              var msg = "Affirmative.\n Second line?";
-              var className = "jquery-console-message-value";
+              
               return [{msg: msg, className: className}];
           }
       },
@@ -90,4 +157,140 @@ function buildConsole()
 function showMessage(msg)
 {
    $("#message-box").append(msg + "\n");
+}
+
+function showWeapons() {
+
+  var weapons = "Your weapons are:\n";
+  $.ajax(
+    {
+        url: '/weapons/' + name,
+        async: false,
+        success: function(data)
+        {
+            console.log("data = " + data);
+            var arr = JSON.parse(data);
+            for(var i = 0; i < arr.length; i++)
+            {
+              weapons += (i + 1) + ". " + arr[i] + "\n";
+            }
+            if(arr.length == 0) {
+              weapons = "You have no weapons left.";
+            }
+        }
+    });
+
+  return weapons;
+
+}
+
+function processCommand(cmd)
+{
+    switch(cmd[1])
+    {
+      case "query":
+        if(queries == 0) return "You don't have any queries left.";
+        var victim = cmd[2];
+        console.log("victim = " + victim);
+        if(!isInt(victim)) {
+          return "Please enter the number of your target.";
+        }
+        if(victim > 36 || victim < 1) {
+          return "Please enter a number between 1 and 36";
+        }
+
+        var owner = processQuery(victim);
+        setButtonText(victim, owner);
+        return "This block belongs to " + owner;
+
+          break;
+      case "shield":
+        var valid = checkWeapon("shield");
+        if (!valid) {
+          return "You don't have this weapon.";
+        }
+        //Notify server of shield
+        socket.emit('shield-me', name);
+        return "One of your blocks has been shielded.";
+
+        break;
+
+      case "deflect":
+        var valid = checkWeapon("deflect");
+        if (!valid) {
+          return "You don't have this weapon.";
+        }
+        //Notify server of shield
+        socket.emit('deflect-me', name);
+        return "One of your blocks has been deflectorized.";
+
+        break;
+
+      case "convert":
+        var valid = checkWeapon("convert");
+        if (!valid) {
+          return "You don't have this weapon.";
+        }
+        //Notify server of shield
+        socket.emit('convert', name);
+        return "You have a new block.";
+
+        break;
+    }
+}
+
+function setButtonText(num, data)
+{
+    $("#button" + num +" .coord").html(data);
+}
+
+function processClick(num)
+{
+
+}
+
+function processQuery(num)
+{
+    var owner = "";
+    $.ajax(
+    {
+      url: '/status/' + num,
+      async: false,
+      success: function(data)
+      {
+        owner = data;
+      }
+    });
+
+    queries--;
+    return owner;
+}
+
+function isInt(data)
+{
+  return data == parseInt(data);
+}
+
+function checkWeapon(weapon)
+{
+
+    var valid = false;
+    $.ajax(
+    {
+        url: '/weapons/' + name,
+        async: false,
+        success: function(data)
+        {
+            console.log("data = " + data);
+            var arr = JSON.parse(data);
+            if(arr.lastIndexOf(weapon) >= 0) {
+              //We have it!
+              valid = true;
+            } else {
+              //Nice try
+              valid = false;
+            }
+        }
+    });
+    return valid;
 }
